b4c7dd79f9b5f9a6160119dba3084910ee9c4471
garvit986 <garvit@binmile.com>
2024-08-23 09:02:50 +0530

    Added Bg

--- src/Screens/Income/Index.tsx
+++ src/Screens/Income/Index.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import { Box, Container, Divider } from "@mui/material";
+import { Box } from "@mui/material";
 import { CommonTableSection } from "../../Components/Common/CommonTableSection";
-import { Transaction, TransactionType } from "../../dbOperations/interfaces";
+import { Transaction } from "../../dbOperations/interfaces";
 import { useSelector } from "react-redux";
 import { selectTransactions } from "../../features/transaction/transactionSlice";
 import { AccountBalance } from "@mui/icons-material";
 import SummaryCard from "../../Components/SummaryCard";
 import AddButton from "../../Components/Common/AddButton";
 import AddIncomeDialog from "../../Components/AddIncome";
-import MiniDrawer from "../../Components/Common/CommonSideBar";
+import CommonTopBar from "../../Components/Common/CommonTopBar";
 import SingleLineChart from "../../Components/SingleLineChart";
 import { processTransactionsForSingleLineChart } from "../../utils/chartUtils";
-import CommonTopBar from "../../Components/Common/CommonTopBar";
 import CommonCard from "../../Components/Common/CommonCard";
+
+// Correct background image URL
+const backgroundImageUrl = "/images/light2.jpeg";
 
 const Incomes: React.FC = () => {
   const trxns = useSelector(selectTransactions);
@@ -34,54 +36,58 @@ const Incomes: React.FC = () => {
   const { amounts, dates } = processTransactionsForSingleLineChart(incomes);
 
   return (
-    <div>
+    <Box
+      sx={{
+        backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        padding: 1,
+        marginLeft: 0,
+        paddingLeft: 0,
+      }}
+    >
       <CommonTopBar title="Income" />
-      <div
-        style={{
-          padding: 1,
-          marginLeft: "0px",
-          paddingLeft: "0px",
-          width: "100%",
+      <Box
+        sx={{
+          maxWidth: "94%",
+          marginTop: "16px",
+          marginLeft: "16px",
+          backgroundColor: "rgba(255, 255, 255, 0.8)", // Optional: Add background color for readability
+          padding: 2,
+          borderRadius: 2,
         }}
       >
-        <div
-          style={{
-            maxWidth: "94%",
-            marginTop: "16px",
-            marginLeft: "16px",
+        <Box component="h3" sx={{ mx: 0 }}>
+          Summary
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
           }}
         >
-          <Box component="h3" sx={{ mx: 0 }}>
-            Summary
-          </Box>
-
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "flex-start",
-            }}
-          >
           <SummaryCard
             value={String(sum)}
             title="Income"
             icon={<AccountBalance sx={{ fontSize: 30, color: "inherit" }} />}
           />
-          </Box>
-          <Box component="h3" sx={{ mx: 0, marginTop: 2 }}>
-            Reports
-          </Box>
-          
-          <CommonCard>
-            <SingleLineChart data={amounts} label="Income" dates={dates} />
-          </CommonCard>
-          <Box sx={{ mt: 2 }}>
-            <CommonTableSection transactions={incomes} type={"Income"} />
-          </Box>
-          <AddButton onClick={handleDialogOpen} />
-          <AddIncomeDialog open={dialogOpen} onClose={handleDialogClose} />
-        </div>
-      </div>
-    </div>
+        </Box>
+        <Box component="h3" sx={{ mx: 0, marginTop: 2 }}>
+          Reports
+        </Box>
+        
+        <CommonCard>
+          <SingleLineChart data={amounts} label="Income" dates={dates} />
+        </CommonCard>
+        <Box sx={{ mt: 2 }}>
+          <CommonTableSection transactions={incomes} type="Income" />
+        </Box>
+        <AddButton onClick={handleDialogOpen} />
+        <AddIncomeDialog open={dialogOpen} onClose={handleDialogClose} />
+      </Box>
+    </Box>
   );
 };
 
